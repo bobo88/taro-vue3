1220d4c8eedeabc518d5b6a35b07e5aaad34cda0
Bob <yuanboi88@163.com>
2024-05-14 15:52:13 +0800

    feat: init commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <view class="index">
+    <view>Taro Demo ~ ycy88.com</view>
+    <Counter />
+  </view>
+</template>
+
+<script setup>
+import "./index.scss";
+import Counter from "../../components/Counter.vue";
+</script>
